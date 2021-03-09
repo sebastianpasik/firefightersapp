7ce5a1d37caea945bc4985284525b6427d02b4f7
Inspironn1525 <pasik.sebastian@gmail.com>
2021-03-09 07:58:43 +0000

    ACCOUNT: catch exceptions when provided password is to weak and when account already exists

--- firefighters_app/lib/screens/login.dart
+++ firefighters_app/lib/screens/login.dart
@@ -109,8 +109,12 @@ class _LoginState extends State<Login> {
                           setState(() {
                             _showSpinner = false;
                           });
-                        } catch (e) {
-                          print(e);
+                           } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            print('No user found for that email.');
+                          } else if (e.code == 'wrong-password') {
+                            print('Wrong password provided for that user.');
+                          }
                         }
                       },
                     ),
--- firefighters_app/lib/screens/registration.dart
+++ firefighters_app/lib/screens/registration.dart
@@ -124,6 +124,12 @@ class _RegistrationState extends State<Registration> {
                           setState(() {
                             _showSpinner = false;
                           });
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            print('The password provided is too weak.');
+                          } else if (e.code == 'email-already-in-use') {
+                            print('The account already exists for that email.');
+                          }
                         } catch (e) {
                           print(e);
                         }
