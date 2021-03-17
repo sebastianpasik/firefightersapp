6e45c572f820afc77ea3c64c2ce3456677a4c4d2
Inspironn1525 <pasik.sebastian@gmail.com>
2021-03-17 08:23:45 +0000

    ACCOUNT: Refactor code. Extract OnClickActionButton widget to a seperate file

--- firefighters_app/lib/screens/settings.dart
+++ firefighters_app/lib/screens/settings.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firefighters_app/screens/login.dart';
+import 'package:firefighters_app/screens/widgets/onclick_action_button.dart';
 import 'package:firefighters_app/utilities/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -10,38 +12,13 @@ class Settings extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        Container(
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 2.0,
-                offset: Offset(1.0, 3.0), // shadow direction: bottom right
-              )
-            ],
-          ),
-          margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-          child: GestureDetector(
-            onTap: () {
-              _auth.signOut();
-              Navigator.pop(context);
-            },
-            child: Container(
-              margin: EdgeInsets.only(top: 12.0, left: 12.0),
-              child: Text(
-                'Log out',
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
-                  color: kMainRedColor,
-                ),
-              ),
-            ),
-          ),
+        OnClickActionButton(
+          label: 'Log out',
+          textStyle: kRedText,
+          onClick: () {
+            _auth.signOut();
+            Navigator.pushReplacementNamed(context, Login.id);
+          },
         ),
       ],
     );
---
+++ firefighters_app/lib/screens/widgets/onclick_action_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class OnClickActionButton extends StatelessWidget {
+  OnClickActionButton(
+      {@required this.label, @required this.onClick, this.textStyle});
+
+  final String label;
+  final Function onClick;
+  final TextStyle textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 2.0,
+            offset: Offset(1.0, 3.0), // shadow direction: bottom right
+          )
+        ],
+      ),
+      margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+      child: GestureDetector(
+        onTap: onClick,
+        child: Container(
+          margin: EdgeInsets.only(top: 12.0, left: 12.0),
+          child: Text(
+            label,
+            textAlign: TextAlign.start,
+            style: textStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
